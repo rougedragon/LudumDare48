6fb8b4f1aa986abb03a0f16d28f45cf2dc16dd81
eperez13 <ethan.perez006@gmail.com>
2021-04-24 18:08:48 +0200

    level system

--- index.py
+++ index.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, session, redirect
 import bs4
 import requests
 import random
 import string
+import json
 
 app = Flask(__name__)
+app.secret_key = "YOUR SECRET KEY HERE"
 
 indexHtml = open("index.html", "r").read()
 menuHtml = open("menu.html", "r").read()
 
+allLevels = []
+with open('levels.json') as json_file:
+    allLevels = json.load(json_file)
+
+
 def getWikipage(wikipage):
-    print(wikipage)
     r = requests.get('https://en.wikipedia.org/wiki/' + wikipage)
     txt = r.text
     soup = bs4.BeautifulSoup(txt)
@@ -25,24 +31,64 @@ def getWikipage(wikipage):
     styleTag = soup.new_tag('style')
     styleTag.insert(0, style)
     soup.head.append(styleTag)
-    
+
     return str(soup)
+
+
+def create_player_id():
+    random_number = random.randrange(1, 20)
+    letter_count, digit_count = random_number, 20 - random_number
+    str1 = ''.join((random.choice(string.ascii_letters)
+                    for x in range(letter_count)))
+    str1 += ''.join((random.choice(string.digits) for x in range(digit_count)))
+
+    sam_list = list(str1)  # it converts the string to list.
+    # It uses a random.shuffle() function to shuffle the string.
+    random.shuffle(sam_list)
+    final_string = ''.join(sam_list)
+    return final_string
+
 
 @app.route('/')
 def index():
     return indexHtml
 
+
 @app.route('/menu')
 def menu():
     return menuHtml
 
+
 @app.route('/<int:level>')
 def level(level):
-    return "this is the level number " + str(level)
+    level = level - 1
+    session["info"] = {
+            "id": create_player_id(),
+            "level": allLevels[level],
+            "stepDone": 0
+        }
+    return redirect("/wiki/" + session["info"]["level"]["start"])
+
 
 @app.route('/wiki/<wikipage>')
 def wikipage(wikipage):
-    return getWikipage(wikipage)
+    if "info" in session and session["info"]["level"] != {}:
+        session["info"] = {
+            "id": session["info"]["id"],
+            "level": session["info"]["level"],
+            "stepDone": session["info"]["stepDone"] + 1
+        }
+        if session["info"]["level"]["end"] == wikipage:
+            session["info"]["level"] = {}
+            return 'You win. Go back to the <a href="/menu">menu</a> to start a new level'
+
+        else:
+            if session["info"]["stepDone"] > session["info"]["level"]["step"]:
+                return 'You loose this level. You have made too many step. Go back to the <a href="/menu">menu</a> to start a new level'
+            return getWikipage(wikipage)
+    else:
+        return "You need to start a level before"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
