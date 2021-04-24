6bf51411ad69ffadfd41cebd86b6a7acc4ab2042
eperez13 <ethan.perez006@gmail.com>
2021-04-25 00:16:04 +0200

    score added

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ with open('levels.json') as json_file:
     allLevels = json.load(json_file)
 
 
-def getWikipage(wikipage, target):
+def getWikipage(wikipage, target, score, link_left):
     r = requests.get('https://en.wikipedia.org/wiki/' + wikipage)
     txt = r.text
     soup = bs4.BeautifulSoup(txt)
@@ -36,7 +36,7 @@ def getWikipage(wikipage, target):
     searchBar = soup.find(id='simpleSearch')
     searchBar.decompose()
 
-    extraSoup = bs4.BeautifulSoup('<div id="mw-head" style="color:red;text-align:center;font-size:xx-large;">TARGET: ' + target + '<br>SCORE: 15</div>')
+    extraSoup = bs4.BeautifulSoup('<div id="mw-head" style="color:red;text-align:center;font-size:x-large;">TARGET: ' + target + '<br>SCORE: ' + str(score) + '<br>LINK LEFT: ' + str(link_left) + '</div>')
     soup.find(id='mw-head').replace_with(extraSoup)
     #targetElt.insert(0, "TARGET: " + target)
 
@@ -69,7 +69,8 @@ def menu():
                 "id": create_player_id(),
                 "level": {},
                 "levelNb": 0,
-                "stepDone": 0
+                "stepDone": 0,
+                "score": 0
             }
         return redirect("/0")
     else:
@@ -79,21 +80,22 @@ def menu():
             "level": session["info"]["level"],
             "stepDone": session["info"]["stepDone"],
             "levelNb": session["info"]["levelNb"] + 1,
+            "score": session["info"]["score"]
         }
         print(session["info"]["levelNb"])
         return redirect("/" + str(session["info"]["levelNb"]))
 
 @app.route('/<int:level>')
 def level(level):
-    level = level - 1
     session["info"] = {
         "id": session["info"]["id"],
         "level": allLevels[level],
         "stepDone": 0,
         "levelNb": session["info"]["levelNb"],
+        "score": session["info"]["score"]
     }
     session["info"]["level"] = allLevels[level]
-    return '<p>You start at ' + session["info"]["level"]["start"] + ' and you end at ' + session["info"]["level"]["end"] + '. You have a maximum of ' + str(session["info"]["level"]["step"]) + ' link to click.</p> <a href="/wiki/' + session["info"]["level"]["start"] + '">START</a>'
+    return '<p>You start at ' + session["info"]["level"]["start"] + ' and you end at ' + session["info"]["level"]["end"] + '. You have a maximum of ' + str(session["info"]["level"]["maxStep"]) + ' link to click but you can do it in ' + str(session["info"]["level"]["maxStep"]) + ' step.</p> <a href="/wiki/' + session["info"]["level"]["start"] + '">START</a>'
 
 
 @app.route('/wiki/<wikipage>')
@@ -103,16 +105,26 @@ def wikipage(wikipage):
             "id": session["info"]["id"],
             "level": session["info"]["level"],
             "levelNb": session["info"]["levelNb"],
-            "stepDone": session["info"]["stepDone"] + 1
+            "stepDone": session["info"]["stepDone"] + 1,
+            "score": session["info"]["score"]
         }
         if session["info"]["level"]["end"] == wikipage:
+            scoreToWin = abs(session["info"]["stepDone"] - session["info"]["level"]["maxStep"])
+            session["info"] = {
+                "id": session["info"]["id"],
+                "level": session["info"]["level"],
+                "levelNb": session["info"]["levelNb"],
+                "stepDone": session["info"]["stepDone"],
+                "score": session["info"]["score"] + scoreToWin
+            }
             session["info"]["level"] = {}
             return 'You win. Click <a href="/menu">here</a> to start a new level'
 
         else:
-            if session["info"]["stepDone"] > session["info"]["level"]["step"]:
+            if session["info"]["stepDone"] > session["info"]["level"]["maxStep"]:
+                
                 return 'You loose this level. You have made too many step. Go back to the <a href="/menu">menu</a> to start a new level'
-            return getWikipage(wikipage, session["info"]["level"]["end"])
+            return getWikipage(wikipage, session["info"]["level"]["end"], session["info"]["score"], (session["info"]["level"]["maxStep"] - session["info"]["stepDone"]) + 1)
     else:
         return "You need to start a level before"
 
