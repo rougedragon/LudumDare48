89b075eb20a69316e815505cc117ea331cbb1824
eperez13 <ethan.perez006@gmail.com>
2021-04-24 23:49:28 +0200

    menu system changed

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, redirect
+from flask import Flask, session, redirect, render_template
 import bs4
 import requests
 import random
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.secret_key = "YOUR SECRET KEY HERE"
 
 indexHtml = open("index.html", "r").read()
-menuHtml = open("menu.html", "r").read()
 
 allLevels = []
 with open('levels.json') as json_file:
@@ -38,7 +37,7 @@ def getWikipage(wikipage, target):
     searchBar.decompose()
 
     extraSoup = bs4.BeautifulSoup('<div id="mw-head" style="color:red;text-align:center;font-size:xx-large;">TARGET: ' + target + '<br>SCORE: 15</div>')
-    print(soup.find(id='mw-head').replace_with(extraSoup))
+    soup.find(id='mw-head').replace_with(extraSoup)
     #targetElt.insert(0, "TARGET: " + target)
 
     return str(soup)
@@ -65,17 +64,35 @@ def index():
 
 @app.route('/menu')
 def menu():
-    return menuHtml
-
+    if not "info" in session:
+        session["info"] = {
+                "id": create_player_id(),
+                "level": {},
+                "levelNb": 0,
+                "stepDone": 0
+            }
+        return redirect("/0")
+    else:
+        print(session["info"])
+        session["info"] = {
+            "id": session["info"]["id"],
+            "level": session["info"]["level"],
+            "stepDone": session["info"]["stepDone"],
+            "levelNb": session["info"]["levelNb"] + 1,
+        }
+        print(session["info"]["levelNb"])
+        return redirect("/" + str(session["info"]["levelNb"]))
 
 @app.route('/<int:level>')
 def level(level):
     level = level - 1
     session["info"] = {
-            "id": create_player_id(),
-            "level": allLevels[level],
-            "stepDone": 0
-        }
+        "id": session["info"]["id"],
+        "level": allLevels[level],
+        "stepDone": 0,
+        "levelNb": session["info"]["levelNb"],
+    }
+    session["info"]["level"] = allLevels[level]
     return '<p>You start at ' + session["info"]["level"]["start"] + ' and you end at ' + session["info"]["level"]["end"] + '. You have a maximum of ' + str(session["info"]["level"]["step"]) + ' link to click.</p> <a href="/wiki/' + session["info"]["level"]["start"] + '">START</a>'
 
 
@@ -85,11 +102,12 @@ def wikipage(wikipage):
         session["info"] = {
             "id": session["info"]["id"],
             "level": session["info"]["level"],
+            "levelNb": session["info"]["levelNb"],
             "stepDone": session["info"]["stepDone"] + 1
         }
         if session["info"]["level"]["end"] == wikipage:
             session["info"]["level"] = {}
-            return 'You win. Go back to the <a href="/menu">menu</a> to start a new level'
+            return 'You win. Click <a href="/menu">here</a> to start a new level'
 
         else:
             if session["info"]["stepDone"] > session["info"]["level"]["step"]:
